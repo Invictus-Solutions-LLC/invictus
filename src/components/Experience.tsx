421fbb0ba4698c9022436273f1ec4a675f045192
Invictus808 <xinvictus808x@gmail.com>
2023-03-18 23:44:44 -1000

    [update]: experience - standardization

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,7 +7,6 @@ type Props = {};
 function Experience({ }: Props) {
     return (
         <motion.div
-            className='h-screen flex relative overflow-hidden flex-col text-left md:flex:row max-w-full px-10 justify-evenly mx-auto items-center'
             initial={{
                 opacity: 0,
             }}
@@ -17,6 +16,7 @@ function Experience({ }: Props) {
             transition={{
                 duration: 1.5,
             }}
+            className='h-screen flex relative overflow-hidden flex-col text-left md:flex:row max-w-full px-10 justify-evenly mx-auto items-center'
         >
             <h3
                 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'
