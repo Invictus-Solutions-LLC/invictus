2dca64cb3a333986fdb9a002c42e5c10a2881771
Invictus808 <xinvictus808x@gmail.com>
2023-03-21 02:48:48 -1000

    [update]: skills - styling

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -16,19 +16,21 @@ function Skills({ }: Props) {
             transition={{
                 duration: 1.5,
             }}
-            className='relative flex flex-col min-h-screen text-center max-w-[2000px] px-10 pb-36 mx-auto items-center'
+            className='relative flex flex-col min-h-screen md:h-screen text-center md:text-left md:flex-row max-w-[2000px] px-10 pb-36 mx-auto justify-evenly items-center'
         >
-            <h3
-                className='static uppercase tracking-[20px] text-gray-500 text-2xl pt-32 pb-4 md:pt-16 md:pb-8 xl:pt-20 xl:pb-12 z-20'
-            >
-                Skills
-            </h3>
+            <div>
+                <h3
+                    className='static uppercase tracking-[20px] text-gray-500 text-2xl pt-32 pb-4 md:pt-16 md:pb-8 xl:pt-20 xl:pb-12 z-20'
+                >
+                    Skills
+                </h3>
 
-            <h3
-                className='static uppercase tracking-[3px] text-gray-500 text-sm'
-            >
-                Hover over a skill for current proficiency
-            </h3>
+                <h3
+                    className='static uppercase tracking-[3px] text-gray-500 text-sm'
+                >
+                    Hover over a skill for current proficiency
+                </h3>
+            </div>
 
             <div
                 className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-5 mt-10 mx-auto'
