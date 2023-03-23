51a3c3cbbe2f2178779cf8a8cfa3e352b32754cb
Invictus808 <xinvictus808x@gmail.com>
2023-03-23 00:09:55 -1000

    [update]: skill - styling

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -37,7 +37,7 @@ function Skill({ directionLeft }: Props) {
                 transition={{
                     duration: 1,
                 }}
-                className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'
+                className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-full h-full rounded-full z-0'
             >
                 <div
                     className='flex items-center justify-center h-full'
