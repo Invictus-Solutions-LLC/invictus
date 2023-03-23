5fbc7abc1f0a1f2ef9c3821c7a37f774072810d0
Invictus808 <xinvictus808x@gmail.com>
2023-03-22 20:37:45 -1000

    [update]: projects - styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,23 +17,23 @@ function Projects({ }: Props) {
             transition={{
                 duration: 1.5,
             }}
-            className='min-h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'
+            className='relative flex flex-col min-h-screen md:h-screen text-center max-w-full mx-auto px-10 pb-36 md:pb-10 items-center'
         >
             <h3
-                className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'
+                className='static uppercase tracking-[20px] text-gray-500 text-2xl pt-32 pb-4 md:pt-16 md:pb-8 xl:pt-20 xl:pb-12 z-20'
             >
                 Projects
             </h3>
 
             <div
-                className='relative w-full h-screen flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scroll-smooth scrollbar-track-gray-400/20 scrollbar-thumb-[#FF0000]/80'
+                className='static flex w-full h-full overflow-x-scroll snap-x snap-mandatory scrollbar-thin scroll-smooth scrollbar-track-gray-400/20 scrollbar-thumb-[#FF0000]/80 z-10'
             >
                 {
                     projects.map((project, index) => {
                         return (
                             <div
                                 key={index}
-                                className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'
+                                className='flex flex-col w-full flex-shrink-0 snap-center space-y-5 items-center justify-center'
                             >
                                 <motion.img
                                     src='https://gitlab.com/uploads/-/system/user/avatar/11223647/avatar.png?width=400'
@@ -58,7 +58,7 @@ function Projects({ }: Props) {
                                     className='space-y-10 px-0 md:px-10 max-w-6xl'
                                 >
                                     <h4
-                                        className='text-4xl font-semibold text-center'
+                                        className='text-2xl md:text-4xl font-semibold text-center'
                                     >
                                         <span
                                             className='underline decoration-[#FF0000]/50'
@@ -69,7 +69,7 @@ function Projects({ }: Props) {
                                     </h4>
 
                                     <p
-                                        className='text-lg text-center md:text-left'
+                                        className='text-md md:text-lg text-center md:text-left'
                                     >
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -84,7 +84,7 @@ function Projects({ }: Props) {
             </div>
 
             <div
-                className='w-full absolute top-[30%] bg-[#FF0000]/10 left-0 h-[500px] -skew-y-12'
+                className='w-full absolute top-[30%] bg-[#FF0000]/10 left-0 h-[500px] -skew-y-12 z-0'
             />
         </motion.div>
     );
